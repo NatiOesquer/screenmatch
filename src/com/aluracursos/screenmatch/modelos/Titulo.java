90b314a022bb331ca37f732d4a99eecf4a494984
NatiOesquer <nati.oesquer@hotmail.com>
2024-04-22 03:59:29 -0300

    creacion de aplicacion de screenmatch

---
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -0,0 +1,60 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Titulo {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluciones;
+    private int totalDeLasEvaluciones;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public int getTotalDeLasEvaluciones(){
+        return totalDeLasEvaluciones;
+    }
+    public void muestraFichaTecnica(){
+        System.out.println("El título es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duración en minutos: " + getDuracionEnMinutos());
+    }
+
+    public void evalua(double nota){
+        sumaDeLasEvaluciones += nota;
+        totalDeLasEvaluciones++;
+    }
+
+    public double calculaMedia(){
+        return sumaDeLasEvaluciones / totalDeLasEvaluciones;
+    }
+}
